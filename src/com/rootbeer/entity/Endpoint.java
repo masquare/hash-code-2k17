1b7b457b4ef65f246ebc762b538dd61923fc56a2
BenKing <ksbe.king@gmail.com>
2017-02-23 18:09:58 +0000

    Basic Addition of Endpoint
    Contains
    cacheDistances
    serverDistance
    requests

---
+++ src/com/rootbeer/entity/Endpoint.java
@@ -0,0 +1,45 @@
+package com.rootbeer.entity;
+
+/**
+ * Created by BenKing on 23/02/2017.
+ */
+public class Endpoint {
+    private int[] cacheDistances;
+    private int distanceToDataCenter;
+    private int[] requests;
+
+
+    public Endpoint(int[] cacheDistances, int dataServerDistance, int[] requests) {
+        this.cacheDistances = cacheDistances;
+        this.distanceToDataCenter = dataServerDistance;
+        this.requests = requests;
+    }
+
+    public void setDistanceToCache(int[] distances){
+        this.cacheDistances = distances;
+    }
+
+    public void setDistanceToDataCenter(int distance){
+        this.distanceToDataCenter = distance;
+    }
+
+    public void setRequests(int[] requests){
+        this.requests = requests;
+    }
+
+    public int[] getDistancesToCache(){
+        return this.cacheDistances;
+    }
+
+    public int getDistanceToCache(int cacheServerID){
+        return this.cacheDistances[cacheServerID];
+    }
+
+    public int getDistanceToDataCenter(){
+        return this.distanceToDataCenter;
+    }
+
+    public int[] getRequests(){
+        return this.requests;
+    }
+}
